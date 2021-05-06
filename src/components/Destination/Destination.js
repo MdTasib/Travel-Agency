ab780e66b0d688c89718a0a4a0e9af00ec24eb14
MdTasib <ohidulalam442@gmail.com>
2021-05-06 12:35:02 +0600

    Home page

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import style from './destination.module.css';
 
 const Destination = ({ data }) => {
     const { image, name } = data;
     return (
-        <div className="col-md-3 text-center mt-5 pt-5">
-            <div>
+        <div className={`${style.destination} col-md-3 text-center p-5`}>
+            <div onClick={() => console.log('hello')} className={`${style.destinationSingle}`}>
                 <img style={{ width: '150px' }} src={image} alt="trien" />
                 <h5>{name}</h5>
             </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import datas from '../FakeData/Data';
 import Destination from '../Destination/Destination';
+import style from './home.module.css';
 
 const Home = () => {
     return (
-        <div className='row text-center'>
+        <div className={`${style.home} row text-center p-5`}>
             {
                 datas.map((data, index) => <Destination key={index} data={data} />)
             }
